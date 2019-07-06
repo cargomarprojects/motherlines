3efbbfdcf47e5fe305067940de9b23ecff2070a2
Joy <cargomarprojects@outlook.com>
2019-07-06 14:52:24 +0530

    save

---
+++ src/app/usaccounts/usaccounts.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
+
+import { USAccountsRoutingModule } from './usaccounts-routing.module';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    SharedModule,
+    USAccountsRoutingModule
+  ]
+})
+export class USAccountsModule { }
