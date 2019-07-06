2b0d253139e147fef1c7ef3d2414ecf54d18cfc8
Joy <cargomarprojects@outlook.com>
2019-07-06 19:05:26 +0530

    save

--- src/app/usaccounts/invoice/invoice.component.ts
+++ src/app/usaccounts/invoice/invoice.component.ts
@@ -23,12 +23,22 @@ export class InvoiceComponent implements OnInit {
   private id: string;
   private menuid: string;
 
-
   private title: string;
   private isAdmin: boolean;
   private canAdd: boolean;
   private canEdit: boolean;
   private canSave: boolean;
+
+  income: number = 0;
+  expense: number = 0;
+  profit: number = 0;
+  ar_bal: number = 0;
+  ap_bal: number = 0;
+
+  MBL_LOSS_APPROVED : boolean = false;
+  MBL_PROFIT_REQ : boolean = false;
+  MBL_LOSS_MEMO : string = '';
+
 
   records: Tbl_cargo_invoicem[]
 
@@ -49,7 +59,7 @@ export class InvoiceComponent implements OnInit {
 
     const options = JSON.parse(params);
 
-    
+
     this.menuid = options.menuid;
     this.mbl_type = options.mbl_type;
     this.mbl_pkid = options.mbl_pkid;
@@ -73,22 +83,70 @@ export class InvoiceComponent implements OnInit {
     SearchData.action = 'NEW';
     SearchData.MBL_PKID = this.mbl_pkid;
     SearchData.INV_TYPE = this.mbl_type;
-    SearchData.ISADMIN= (this.isAdmin) ? 'Y' : 'NO';
+    SearchData.ISADMIN = (this.isAdmin) ? 'Y' : 'NO';
     SearchData.SHOWDELETED = this.showdeleted;
     SearchData.BR_REGION = this.gs.BRANCH_REGION;
 
     SearchData.page_count = 0;
     SearchData.page_rows = 0;
     SearchData.page_current = -1;
+    SearchData.page_rowcount = 0;
 
 
 
     this.mainservice.List(SearchData).subscribe(response => {
       this.records = response.list;
+
+      this.MBL_LOSS_APPROVED = response.MBL_LOSS_APPROVED;
+      this.MBL_PROFIT_REQ =response.MBL_PROFIT_REQ;
+      this.MBL_LOSS_MEMO =response.MBL_LOSS_MEMO;
+
+      this.DisplayProfit();
     }, error => {
       this.errormessage = this.gs.getError(error)
     });
   }
+
+
+  DisplayProfit() {
+    var nInc = 0;
+    var nExp = 0;
+    var nProfit = 0;
+
+    var nAr_Bal = 0;
+    var nAp_Bal = 0;
+
+    if (this.records == null)
+      return;
+
+    this.records.forEach(Rec => {
+      if (Rec.rec_deleted == "N") {
+        if (Rec.inv_arap == "AR") {
+          nInc += Rec.inv_ar_total;
+          nAr_Bal += Rec.inv_balance;
+        }
+        else {
+          nExp += Rec.inv_ap_total;
+          nAp_Bal += Rec.inv_balance;
+        }
+      }
+    })
+    
+    
+    
+    nProfit = this.gs.roundNumber(nInc - nExp,2);
+    this.income = this.gs.roundNumber(nInc,2);
+    this.expense = this.gs.roundNumber(nExp,2);
+    this.profit = this.gs.roundNumber(nProfit,2);
+    this.ar_bal = this.gs.roundNumber(nAr_Bal,2);
+    this.ap_bal = this.gs.roundNumber(nAp_Bal,2);
+    
+
+  }
+
+
+
+
 
 
   NewRecord() {
