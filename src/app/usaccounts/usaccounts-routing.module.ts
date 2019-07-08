b1921817e6bffbf3da78392f7b6e957b8de57346
Joy <cargomarprojects@outlook.com>
2019-07-08 17:41:26 +0530

    save

--- src/app/usaccounts/usaccounts-routing.module.ts
+++ src/app/usaccounts/usaccounts-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { InvoiceComponent } from './invoice/invoice.component';
+import { InvoiceEditComponent } from './invoice/edit/invoice-edit.component';
 
 
 const routes: Routes = [
   { path : 'InvoicePage', component : InvoiceComponent },
+  { path : 'InvoiceEditPage', component : InvoiceEditComponent },
 ];
 
 @NgModule({
