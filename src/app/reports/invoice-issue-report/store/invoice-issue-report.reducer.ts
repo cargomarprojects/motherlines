0571a08a63d194a8566c068573000d35c0c277e4
Joy <cargomarprojects@outlook.com>
2019-08-29 15:09:50 +0530

    save

---
+++ src/app/reports/invoice-issue-report/store/invoice-issue-report.reducer.ts
@@ -0,0 +1,55 @@
+import { createFeatureSelector,createSelector } from '@ngrx/store';
+import { TBL_MBL_REPORT } from '../../models/tbl_mbl_report';
+import { AppState } from '../../../reducer';
+
+import * as myActions from './invoice-issue-report.actions';
+import { ReportState } from './invoice-issue-report.models';
+
+export interface AppState extends AppState {
+    'InvoiceIssueReport': ReportState
+}
+
+export const initialState: ReportState = {
+    pkid  : '',
+    urlid : '',
+    menuid : '',
+    currentTab :'',
+    report_category:'',
+    sdate :'',
+    edate :'',
+    mode : '',
+    comp_type : '',
+    report_type : '',
+    report_shptype : '',
+    cons_id :'',
+    cons_name :'',
+    reportformat :'',
+    page_rows :0,
+    page_count :0,
+    page_current : 0,
+    page_rowcount :0,
+    records : []
+};
+
+export function InvoiceIssueReportReducer(state: ReportState[] = [initialState], action: myActions.Actions): ReportState [] {
+  switch (action.type) {
+    case myActions.ActionTypes.ADD:
+        return [...state,action.payload];
+    case myActions.ActionTypes.UPDATE:
+        return [...state.filter(rec => rec.urlid != action.payload.id), action.payload.changes];
+    case myActions.ActionTypes.DELETE:
+        return [...state.filter(rec => rec.urlid != action.payload.id)];
+    default:
+      return state;
+  }
+}
+
+export const getReducer = createFeatureSelector<ReportState[]>('InvoiceIssueReport');
+
+export const getState = (urlid : string ) => createSelector(
+    getReducer,
+    (state : ReportState[]) =>  state.find( rec => rec.urlid == urlid)
+);
+
+
+ 
