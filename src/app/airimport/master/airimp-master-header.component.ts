a270f7f85e412f8543348edd407e03bf24f52896
joy <joycok@gmail.com>
2019-09-02 11:29:10 +0530

    airdate

--- src/app/airimport/master/airimp-master-header.component.ts
+++ src/app/airimport/master/airimp-master-header.component.ts
@@ -1,30 +1,37 @@
-import { Component, OnInit, Input, Output, EventEmitter,SimpleChange, ChangeDetectionStrategy } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChange, ChangeDetectionStrategy } from '@angular/core';
+import { GlobalService } from '../../core/services/global.service';
 import { SearchQuery } from '../models/tbl_cargo_imp_masterm';
 
 @Component({
   selector: 'app-airimp-master-header',
   templateUrl: './airimp-master-header.component.html',
-  changeDetection : ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AirImpMasterHeaderComponent implements OnInit {
   // Call By Value using Input Parameters
-  searchQuery : SearchQuery;
-  @Input() set _query( value : SearchQuery){
-    this.searchQuery  = Object.assign({}, value);
+  searchQuery: SearchQuery;
+  @Input() set _query(value: SearchQuery) {
+    this.searchQuery = Object.assign({}, value);
   }
-  
+
   @Output() searchEvents = new EventEmitter<any>();
 
-  constructor() { }
+  constructor(public gs: GlobalService
+  ) { }
 
   ngOnInit() {
   }
 
-  ngOnChanges(changes:  SimpleChange ) {
+  ngOnChanges(changes: SimpleChange) {
   }
 
   List(outputformat: string) {
+
+    if (this.gs.isBlank(this.searchQuery.fromdate))
+      this.searchQuery.fromdate = this.gs.year_start_date;
+    if (this.gs.isBlank(this.searchQuery.todate))
+      this.searchQuery.todate = this.gs.defaultValues.today;
+
     this.searchEvents.emit({ outputformat: outputformat, searchQuery: this.searchQuery });
   }
 }
