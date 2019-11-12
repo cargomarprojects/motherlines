8a287729053b4942123de7bb53ce0128faa73c1e
Joy <joycok@gmail.com>
2019-11-12 12:42:53 +0530

    save

---
+++ src/app/other/services/mblusage.service.ts
@@ -0,0 +1,182 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject } from 'rxjs';
+import { GlobalService } from '../../core/services/global.service';
+import { Tbl_cargo_mblusage, Tbl_cargo_mblusageModel } from '../models/Tbl_cargo_mblusage';
+import { SearchQuery } from '../models/Tbl_cargo_mblusage';
+import { PageQuery } from '../../shared/models/pageQuery';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class MblUsageService {
+
+    private mdata$ = new BehaviorSubject<Tbl_cargo_mblusageModel>(null);
+    get data$(): Observable<Tbl_cargo_mblusageModel> {
+        return this.mdata$.asObservable();
+    }
+    private record: Tbl_cargo_mblusageModel;
+
+    public id: string;
+    public menuid: string;
+    public param_type: string;
+
+    public title: string;
+    public isAdmin: boolean;
+    public canAdd: boolean;
+    public canEdit: boolean;
+    public canSave: boolean;
+    public canDelete: boolean;
+
+    public initlialized: boolean;
+
+
+    constructor(
+        private http2: HttpClient,
+        private gs: GlobalService
+    ) { }
+
+    public init(params: any) {
+        if (this.initlialized)
+            return;
+
+        this.id = params.id;
+        this.menuid = params.id;
+        this.param_type = params.param_type;
+
+        this.record = <Tbl_cargo_mblusageModel>{
+            errormessage: '',
+            records: [],
+            searchQuery: <SearchQuery>{ searchString: ''},
+            pageQuery: <PageQuery>{ action: 'NEW', page_count: 0, page_current: -1, page_rowcount: 0, page_rows: 0 }
+        };
+
+        this.mdata$.next(this.record);
+
+        this.isAdmin = this.gs.IsAdmin(this.menuid);
+        this.title = this.gs.getTitle(this.menuid);
+        this.canAdd = this.gs.canAdd(this.menuid);
+        this.canEdit = this.gs.canEdit(this.menuid);
+        this.canDelete = this.gs.canDelete(this.menuid);        
+        this.canSave = this.canAdd || this.canEdit;
+
+        this.initlialized = true;
+
+    }
+
+    Search(_searchdata: any, type: string = '') {
+
+        if (type == 'SEARCH') {
+            this.record.searchQuery = _searchdata.searchQuery;
+        }
+        if (type == 'PAGE') {
+            this.record.pageQuery = _searchdata.pageQuery;
+        }
+
+        var SearchData = this.gs.UserInfo;
+        SearchData.outputformat = 'SCREEN';
+        SearchData.action = 'NEW';
+        SearchData.pkid = this.id;
+        SearchData.TYPE = 'FT';
+        SearchData.page_rowcount = this.gs.ROWS_TO_DISPLAY;
+        SearchData.CODE = this.record.searchQuery.searchString;
+
+        SearchData.FDATE = this.record.searchQuery.sdate;
+        SearchData.EDATE = this.record.searchQuery.edate;
+        SearchData.YEAR = this.gs.year_code;
+        
+
+
+        SearchData.page_count = 0;
+        SearchData.page_rows = 0;
+        SearchData.page_current = -1;
+
+        if (type == 'PAGE') {
+            SearchData.action = this.record.pageQuery.action;
+            SearchData.page_count = this.record.pageQuery.page_count;
+            SearchData.page_rows = this.record.pageQuery.page_rows;
+            SearchData.page_current = this.record.pageQuery.page_current;;
+        }
+
+        this.List(SearchData).subscribe(response => {
+            this.record.pageQuery = <PageQuery>{ action: 'NEW', page_rows: response.page_rows, page_count: response.page_count, page_current: response.page_current, page_rowcount: response.page_rowcount };
+            this.record.records = response.list;
+            this.mdata$.next(this.record);
+        }, error => {
+            this.record = <Tbl_cargo_mblusageModel>{
+                records: [],
+                errormessage: this.gs.getError(error),
+            }
+            this.mdata$.next(this.record);
+        });
+    }
+
+    RefreshList(_rec: Tbl_cargo_mblusage) {
+        if (this.record.records == null)
+            return;
+        var REC = this.record.records.find(rec => rec.mu_pkid == _rec.mu_pkid);
+        if (REC == null) {
+            this.record.records.push(_rec);
+        }
+        else {
+            REC.mu_agent_name = _rec.mu_agent_name;
+            REC.mu_bl_start_no = _rec.mu_bl_start_no;
+            REC.mu_bl_end_no = _rec.mu_bl_end_no;
+            REC.mu_bl_tot = _rec.mu_bl_tot;
+            REC.mu_origin = _rec.mu_origin;
+            REC.mu_courier_name = _rec.mu_courier_name;
+            REC.mu_tracking_no = _rec.mu_tracking_no;
+            REC.mu_sent_on = _rec.mu_sent_on;
+            REC.mu_remarks = _rec.mu_remarks;
+            REC.rec_created_by = _rec.rec_created_by;
+            REC.rec_created_date = _rec.rec_created_date;
+        }
+    }
+    
+    DeleteRow(_rec: Tbl_cargo_mblusage) {
+
+        this.record.errormessage = '';
+        if (!confirm("DELETE " + _rec.mu_tracking_no )) {
+            return;
+        }
+
+        var SearchData = this.gs.UserInfo;
+        SearchData.pkid = _rec.mu_pkid;
+
+        this.DeleteRecord(SearchData)
+            .subscribe(response => {
+                if (response.retvalue == false) {
+                    this.record.errormessage = response.error;
+                    alert(this.record.errormessage);
+                }
+                else {
+                    this.record.records.splice(this.record.records.findIndex(rec => rec.mu_pkid == _rec.mu_pkid), 1);
+                }
+            }, error => {
+                this.record.errormessage = this.gs.getError(error);
+                alert(this.record.errormessage);
+            });
+    }
+
+
+    List(SearchData: any) {
+        return this.http2.post<any>(this.gs.baseUrl + '/api/OblRelease/List', SearchData, this.gs.headerparam2('authorized'));
+    }
+
+    GetRecord(SearchData: any) {
+        return this.http2.post<any>(this.gs.baseUrl + '/api/MblUsage/GetRecord', SearchData, this.gs.headerparam2('authorized'));
+    }
+
+
+    DeleteRecord(SearchData: any) {
+        return this.http2.post<any>(this.gs.baseUrl + '/api/MblUsage/Delete', SearchData, this.gs.headerparam2('authorized'));
+    }
+
+    Save(SearchData: any) {
+        return this.http2.post<any>(this.gs.baseUrl + '/api/MblUsage/Save', SearchData, this.gs.headerparam2('authorized'));
+    }
+
+
+
+
+}
