e2cb734d376d7a4c6da74a76da185e11545c7f47
Joy <joycok@gmail.com>
2019-11-15 15:43:29 +0530

    save

---
+++ src/app/usaccounts-reports/bankstatement/bankstate-header.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChange, ChangeDetectionStrategy } from '@angular/core';
+import { GlobalService } from '../../core/services/global.service';
+import { SearchQuery } from '../models/Tbl_cargo_mblusage';
+
+@Component({
+    selector: 'acc-mblusage-header',
+    templateUrl: './mblusage-header.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class BankStateHeaderComponent implements OnInit {
+    // Call By Value using Input Parameters
+    searchQuery: SearchQuery;
+    @Input() set _query(value: SearchQuery) {
+        this.searchQuery = Object.assign({}, value);
+        this.initData();
+
+    }
+    @Output() searchEvents = new EventEmitter<any>();
+
+    constructor(public gs: GlobalService
+    ) { }
+
+    ngOnInit() {
+
+    }
+
+    initData(){
+        if (this.gs.isBlank(this.searchQuery.sdate))
+            this.searchQuery.sdate = this.gs.defaultValues.lastmonthdate;        
+        if (this.gs.isBlank(this.searchQuery.edate))
+            this.searchQuery.edate = this.gs.defaultValues.today;        
+    }
+
+    ngOnChanges(changes: SimpleChange) {
+    }
+
+    List(outputformat: string) {
+        if (this.gs.isBlank(this.searchQuery.sdate))
+            this.searchQuery.sdate = this.gs.year_start_date;
+        if (this.gs.isBlank(this.searchQuery.edate))
+            this.searchQuery.edate = this.gs.defaultValues.today;
+        this.searchEvents.emit({ outputformat: outputformat, searchQuery: this.searchQuery });
+    }
+}
