7055098aafa7158b71536824079451086fb1c791
Joy <joycok@gmail.com>
2019-12-05 16:50:19 +0530

    sav

--- src/app/usaccounts/services/accsettings.service.ts
+++ src/app/usaccounts/services/accsettings.service.ts
@@ -129,10 +129,6 @@ export class AccSettingsService {
         return this.http2.post<any>(this.gs.baseUrl + '/api/Acctm/GetRecord', SearchData, this.gs.headerparam2('authorized'));
     }
 
-    Save(SearchData: any) {
-        return this.http2.post<any>(this.gs.baseUrl + '/api/Acctm/Save', SearchData, this.gs.headerparam2('authorized'));
-    }
-
     SaveAcctmSettingsSave(SearchData: any) {
         return this.http2.post<any>(this.gs.baseUrl + '/api/Acctm/AcctmSettingsSave', SearchData, this.gs.headerparam2('authorized'));
     }
