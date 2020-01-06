cb495f9a64ecc94a083acfec56668d8eead159c2
joy <joycok@gmail.com>
2020-01-06 16:51:12 +0530

    seabrchng

--- src/app/seaimport/services/seaimp-master.service.ts
+++ src/app/seaimport/services/seaimp-master.service.ts
@@ -32,7 +32,7 @@ export class SeaImpMasterService {
     public canDelete: boolean;
 
     public initlialized: boolean;
-
+    public initlializedBrcode: string = '';
 
     constructor(
         private http2: HttpClient,
@@ -40,6 +40,12 @@ export class SeaImpMasterService {
     ) { }
 
     public init(params: any) {
+        if (this.initlializedBrcode != this.gs.branch_code) {
+            this.initlializedBrcode = this.gs.branch_code;
+            this.initlialized = false;
+            this.record = null;
+            this.mdata$.next(this.record);
+        }
         if (this.initlialized)
             return;
 
