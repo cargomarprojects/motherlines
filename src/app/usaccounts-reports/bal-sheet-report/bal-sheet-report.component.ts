a7db127a3140014b555e52deea92d51a04aef6ba
joy <joycok@gmail.com>
2020-04-07 13:22:34 +0530

    usaccrptexcl

--- src/app/usaccounts-reports/bal-sheet-report/bal-sheet-report.component.ts
+++ src/app/usaccounts-reports/bal-sheet-report/bal-sheet-report.component.ts
@@ -39,6 +39,9 @@ export class BalSheetReportComponent implements OnInit {
   filename: string = '';
   filetype: string = '';
   filedisplayname: string = '';
+  filename2: string = '';
+  filetype2: string = '';
+  filedisplayname2: string = '';
 
   lov_where: string = "";
 
@@ -94,6 +97,9 @@ export class BalSheetReportComponent implements OnInit {
         this.filename = rec.filename;
         this.filetype = rec.filetype;
         this.filedisplayname = rec.filedisplayname;
+        this.filename2 = rec.filename2;
+        this.filetype2 = rec.filetype2;
+        this.filedisplayname2 = rec.filedisplayname2;
 
         this.page_rows = rec.page_rows;
         this.page_count = rec.page_count;
@@ -126,6 +132,9 @@ export class BalSheetReportComponent implements OnInit {
         this.filename = '';
         this.filetype = '';
         this.filedisplayname = '';
+        this.filename2 = '';
+        this.filetype2 = '';
+        this.filedisplayname2 = '';
         this.SearchData = this.gs.UserInfo;
 
       }
@@ -186,6 +195,9 @@ export class BalSheetReportComponent implements OnInit {
       this.SearchData.filename = "";
       this.SearchData.filedisplayname = "";
       this.SearchData.filetype = "";
+      this.SearchData.filename2 = "";
+      this.SearchData.filedisplayname2 = "";
+      this.SearchData.filetype2 = "";
     }
 
     this.loading = true;
@@ -197,6 +209,9 @@ export class BalSheetReportComponent implements OnInit {
             this.SearchData.filename = response.filename;
             this.SearchData.filedisplayname = response.filedisplayname;
             this.SearchData.filetype = response.filetype;
+            this.SearchData.filename2 = response.filename2;
+            this.SearchData.filedisplayname2 = response.filedisplayname2;
+            this.SearchData.filetype2 = response.filetype2;
           }
 
           const state: ReportState = {
@@ -215,7 +230,10 @@ export class BalSheetReportComponent implements OnInit {
             records: response.list,
             filename: this.SearchData.filename,
             filetype: this.SearchData.filetype,
-            filedisplayname: this.SearchData.filedisplayname
+            filedisplayname: this.SearchData.filedisplayname,
+            filename2: this.SearchData.filename2,
+            filetype2: this.SearchData.filetype2,
+            filedisplayname2: this.SearchData.filedisplayname2
           };
           this.store.dispatch(new myActions.Update({ id: this.urlid, changes: state }));
         }
