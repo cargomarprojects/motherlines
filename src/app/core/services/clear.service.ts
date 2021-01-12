c224b8180e2e62bd304d03cf794ab9e0acadb7c7
joy <joycok@gmail.com>
2021-01-12 12:45:35 +0530

    clrse

--- src/app/core/services/clear.service.ts
+++ src/app/core/services/clear.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { GlobalService } from './global.service';
 import { SeaImpMasterService } from '../../seaimport/services/seaimp-master.service';
 import { SeaImpHouseService } from '../../seaimport/services/seaimp-house.service';
+import { HouseService } from '../../seaexport/services/house.service';
+import { seaexpMasterService } from '../../seaexport/services/seaexp-master.service';
 
 @Injectable({
     providedIn: 'root'
@@ -13,13 +15,17 @@ export class ClearService {
         private http2: HttpClient,
         private gs: GlobalService,
         private sim: SeaImpMasterService,
-        private sih: SeaImpHouseService
+        private sih: SeaImpHouseService,
+        private sem: seaexpMasterService,
+        private seh: HouseService
     ) {
     }
 
     ClearInit() {
       this.sim.ClearInit();
       this.sih.ClearInit();
+      this.sem.ClearInit();
+      this.seh.ClearInit();
     }
 }
 
--- src/app/seaexport/services/seaexp-master.service.ts
+++ src/app/seaexport/services/seaexp-master.service.ts
@@ -32,20 +32,30 @@ export class seaexpMasterService {
     public canDelete: boolean;
 
     public initlialized: boolean;
-    public initlializedBrcode: string = '';
+    // public initlializedBrcode: string = '';
     constructor(
         private http2: HttpClient,
         private gs: GlobalService
     ) { }
 
+    public ClearInit() {
+        this.record = <seaExpMasterModel>{
+            errormessage: '',
+            records: [],
+            searchQuery: <SearchQuery>{ searchString: '', fromdate: this.gs.getPreviousDate(this.gs.SEARCH_DATE_DIFF), todate: this.gs.defaultValues.today },
+            pageQuery: <PageQuery>{ action: 'NEW', page_count: 0, page_current: -1, page_rowcount: 0, page_rows: 0 }
+        };
+        this.mdata$.next(this.record);
+    }
+    
     public init(params: any) {
 
-        if (this.initlializedBrcode != this.gs.branch_code) {
-            this.initlializedBrcode = this.gs.branch_code;
-            this.initlialized = false;
-            this.record = null;
-            this.mdata$.next(this.record);
-        }
+        // if (this.initlializedBrcode != this.gs.branch_code) {
+        //     this.initlializedBrcode = this.gs.branch_code;
+        //     this.initlialized = false;
+        //     this.record = null;
+        //     this.mdata$.next(this.record);
+        // }
 
         if (this.initlialized)
             return;
